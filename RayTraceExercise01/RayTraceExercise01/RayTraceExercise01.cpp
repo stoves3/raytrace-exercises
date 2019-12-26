67270fb79e6769d355aace37aa265d07e585b2dc
Steve McKisic <smckisic@dsicovery.com>
2019-12-26 02:40:04 -0600

    refactored collision support for multiple objects

--- RayTraceExercise01/RayTraceExercise01/RayTraceExercise01.cpp
+++ RayTraceExercise01/RayTraceExercise01/RayTraceExercise01.cpp
@@ -2,32 +2,34 @@
 //
 
 #include <iostream>
-#include "ray.h"
+#include "sphere.h"
+#include "hitable_list.h"
+#include "float.h"
 
-float hit_sphere(const vec3& center, float radius, const ray& r) {
-	vec3 oc = r.origin() - center;
-	float a = dot(r.direction(), r.direction());
-	float b = 2.0 * dot(oc, r.direction());
-	float c = dot(oc, oc) - radius * radius;
-	float discriminant = b * b - 4 * a * c;
-	if (discriminant < 0) {
-		return -1.0;
+//float hit_sphere(const vec3& center, float radius, const ray& r) {
+//	vec3 oc = r.origin() - center;
+//	float a = dot(r.direction(), r.direction());
+//	float b = 2.0 * dot(oc, r.direction());
+//	float c = dot(oc, oc) - radius * radius;
+//	float discriminant = b * b - 4 * a * c;
+//	if (discriminant < 0) {
+//		return -1.0;
+//	}
+//	else {
+//		return (-b - sqrt(discriminant)) / (2.0 * a);
+//	}
+//}
+
+vec3 color(const ray& r, hitable *world) {
+	hit_record rec;
+	if (world->hit(r, 0.0, FLT_MAX, rec)) {
+		return 0.5 * vec3(rec.normal.x() + 1, rec.normal.y() + 1, rec.normal.z() + 1);
 	}
 	else {
-		return (-b - sqrt(discriminant)) / (2.0 * a);
+		vec3 unit_direction = unit_vector(r.direction());
+		float t = 0.5 * (unit_direction.y() + 1.0);
+		return (1.0 - t) * vec3(1.0, 1.0, 1.0) + t * vec3(0.5, 0.7, 1.0);
 	}
-}
-
-vec3 color(const ray& r) {
-	float t = hit_sphere(vec3(0, 0, -1), 0.5, r);
-	if (t > 0.0) {
-		vec3 N = unit_vector(r.point_at_parameter(t) - vec3(0, 0, -1));
-		return 0.5 * vec3(N.x() + 1, N.y() + 1, N.z() + 1);
-	}
-
-	vec3 unit_direction = unit_vector(r.direction());
-	t = 0.5 * (unit_direction.y() + 1.0);
-	return (1.0 - t) * vec3(1.0, 1.0, 1.0) + t * vec3(0.5, 0.7, 1.0);
 }
 
 int main()
@@ -40,7 +42,10 @@ int main()
 	vec3 horizontal(4.0, 0.0, 0.0);
 	vec3 vertical(0.0, 2.0, 0.0);
 	vec3 origin(0.0, 0.0, 0.0);
-
+	hitable* list[2];
+	list[0] = new sphere(vec3(0, 0, -1), 0.5);
+	list[1] = new sphere(vec3(0, -100.5, -1), 100);
+	hitable* world = new hitable_list(list, 2);
 	for (int j = ny-1; j >= 0; j--)
 	{
 		for (int i = 0; i < nx; i++)
@@ -53,7 +58,8 @@ int main()
 			float g = float(j) / float(ny);
 			float b = 0.2;*/
 			//vec3 col(float(i) / float(nx), float(j) / float(ny), 0.2);
-			vec3 col = color(r);
+			vec3 p = r.point_at_parameter(2.0);
+			vec3 col = color(r, world);
 
 			/*int ir = int(255.99 * r);
 			int ig = int(255.99 * g);
