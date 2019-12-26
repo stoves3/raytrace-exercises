8ebf5b30ee023a399e8946f4afb3317fb83ce84d
Steve McKisic <smckisic@dsicovery.com>
2019-12-25 23:49:01 -0600

    added vector and ray classes updated to trace color

--- RayTraceExercise01/RayTraceExercise01/RayTraceExercise01.cpp
+++ RayTraceExercise01/RayTraceExercise01/RayTraceExercise01.cpp
@@ -2,6 +2,13 @@
 //
 
 #include <iostream>
+#include "ray.h"
+
+vec3 color(const ray& r) {
+	vec3 unit_direction = unit_vector(r.direction());
+	float t = 0.5 * (unit_direction.y() + 1.0);
+	return (1.0 - t) * vec3(1.0, 1.0, 1.0) + t * vec3(0.5, 0.7, 1.0);
+}
 
 int main()
 {
@@ -9,16 +16,31 @@ int main()
 	int ny = 100;
 
 	std::cout << "P3\n" << nx << " " << ny << "\n255\n";
+	vec3 lower_left_corner(-2.0, -1.0, -1.0);
+	vec3 horizontal(4.0, 0.0, 0.0);
+	vec3 vertical(0.0, 2.0, 0.0);
+	vec3 origin(0.0, 0.0, 0.0);
+
 	for (int j = ny-1; j >= 0; j--)
 	{
 		for (int i = 0; i < nx; i++)
 		{
-			float r = float(i) / float(nx);
+			float u = float(i) / float(nx);
+			float v = float(j) / float(ny);
+			ray r(origin, lower_left_corner + u * horizontal + v * vertical);
+
+			/*float r = float(i) / float(nx);
 			float g = float(j) / float(ny);
-			float b = 0.2;
-			int ir = int(255.99 * r);
+			float b = 0.2;*/
+			//vec3 col(float(i) / float(nx), float(j) / float(ny), 0.2);
+			vec3 col = color(r);
+
+			/*int ir = int(255.99 * r);
 			int ig = int(255.99 * g);
-			int ib = int(255.99 * b);
+			int ib = int(255.99 * b);*/
+			int ir = int(255.99 * col[0]);
+			int ig = int(255.99 * col[1]);
+			int ib = int(255.99 * col[2]);
 			std::cout << ir << " " << ig << " " << ib << "\n";
 		}
 	}
