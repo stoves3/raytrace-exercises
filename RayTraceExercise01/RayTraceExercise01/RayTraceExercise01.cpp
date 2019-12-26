6e489fbb39b16f94918e57f4a3b00fa5035f09ad
Steve McKisic <smckisic@dsicovery.com>
2019-12-26 03:31:08 -0600

    diffuse materials

--- RayTraceExercise01/RayTraceExercise01/RayTraceExercise01.cpp
+++ RayTraceExercise01/RayTraceExercise01/RayTraceExercise01.cpp
@@ -4,10 +4,24 @@
 #include "hitable_list.h"
 #include "float.h"
 
+double random_double() {
+	return rand() / (RAND_MAX + 1.0);
+}
+
+vec3 random_in_unit_sphere() {
+	vec3 p;
+	do {
+		p = 2.0 * vec3(random_double(), random_double(), random_double()) - vec3(1, 1, 1);
+	} while (p.squared_length() >= 1.0);
+	return p;
+}
+
+
 vec3 color(const ray& r, hitable *world) {
 	hit_record rec;
-	if (world->hit(r, 0.0, FLT_MAX, rec)) {
-		return 0.5 * vec3(rec.normal.x() + 1, rec.normal.y() + 1, rec.normal.z() + 1);
+	if (world->hit(r, 0.001, FLT_MAX, rec)) {
+		vec3 target = rec.p + rec.normal + random_in_unit_sphere();
+		return 0.5 * color(ray(rec.p, target - rec.p), world);
 	}
 	else {
 		vec3 unit_direction = unit_vector(r.direction());
@@ -38,14 +52,15 @@ int main()
 			vec3 col(0, 0, 0);
 			for (int s = 0; s < ns; s++)
 			{
-				float u = float(i + (rand() / (RAND_MAX + 1.0))) / float(nx);
-				float v = float(j + (rand() / (RAND_MAX + 1.0))) / float(ny);
+				float u = float(i + random_double()) / float(nx);
+				float v = float(j + random_double()) / float(ny);
 
 				ray r = cam.get_ray(u, v);
 				vec3 p = r.point_at_parameter(2.0);
 				col += color(r, world);
 			}
 			col /= float(ns);
+			col = vec3(sqrt(col[0]), sqrt(col[1]), sqrt(col[2]));
 
 			int ir = int(255.99 * col[0]);
 			int ig = int(255.99 * col[1]);
