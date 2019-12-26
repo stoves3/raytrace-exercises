55fbadeb519dd3e4ebe2caf2fe15693264b77061
Steve McKisic <smckisic@dsicovery.com>
2019-12-25 21:46:55 -0600

    hello world!

---
+++ RayTraceExercise01/RayTraceExercise01/RayTraceExercise01.cpp
@@ -0,0 +1,36 @@
+// RayTraceExercise01.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+int main()
+{
+	int nx = 200;
+	int ny = 100;
+
+	std::cout << "P3\n" << nx << " " << ny << "\n255\n";
+	for (int j = ny-1; j >= 0; j--)
+	{
+		for (int i = 0; i < nx; i++)
+		{
+			float r = float(i) / float(nx);
+			float g = float(j) / float(ny);
+			float b = 0.2;
+			int ir = int(255.99 * r);
+			int ig = int(255.99 * g);
+			int ib = int(255.99 * b);
+			std::cout << ir << " " << ig << " " << ib << "\n";
+		}
+	}
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
