5e99dd130d1b814472beead437f6a216ddbd3116
Steve McKisic <smckisic@dsicovery.com>
2019-12-27 03:10:40 -0600

    moveable camera

--- RayTraceExercise01/RayTraceExercise01/RayTraceExercise01.cpp
+++ RayTraceExercise01/RayTraceExercise01/RayTraceExercise01.cpp
@@ -43,7 +43,7 @@ int main()
 	list[4] = new sphere(vec3(-1, 0, -1), -0.45, new dielectric(1.5));
 
 	hitable *world = new hitable_list(list, 4);
-	camera cam;
+	camera cam(vec3(-2,2,1), vec3(0,0,-1), vec3(0,1,0), 90, float(nx)/float(ny));
 
 	for (int j = ny-1; j >= 0; j--)
 	{
