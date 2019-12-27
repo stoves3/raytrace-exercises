3d35f54411f26e4032d9062e363640d3e16eca46
Steve McKisic <smckisic@dsicovery.com>
2019-12-27 03:35:43 -0600

    depth of field / defocus blur

--- RayTraceExercise01/RayTraceExercise01/core.h
+++ RayTraceExercise01/RayTraceExercise01/core.h
@@ -14,6 +14,14 @@ inline vec3 random_in_unit_sphere() {
 	return p;
 }
 
+inline vec3 random_in_unit_disk() {
+	vec3 p;
+	do {
+		p = 2.0 * vec3(random_double(), random_double(), 0) - vec3(1, 1, 0);
+	} while (dot(p, p) >= 1.0);
+	return p;
+}
+
 inline vec3 reflect(const vec3& v, const vec3& n) {
 	return v - 2 * dot(v, n) * n;
 }
